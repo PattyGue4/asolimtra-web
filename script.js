e5ce2dd123276b6fa265940d9eedb3e6341ada99
Patricia Guevara <pguevara404@gmail.com>
2026-06-22 18:08:17 -0500

    Pagina web ASOLIMTRA

---
+++ script.js
@@ -0,0 +1,59 @@
+function enviarFormulario(event) {
+  event.preventDefault();
+
+  let nombre = document.getElementById("nombre").value;
+  let institucion = document.getElementById("institucion").value;
+  let telefono = document.getElementById("telefono").value;
+  let correo = document.getElementById("correo").value;
+  let tipo = document.getElementById("tipo").value;
+  let mensaje = document.getElementById("mensaje").value;
+
+  let texto = `Hola ASOLIMTRA, mi nombre es ${nombre}.
+Institución: ${institucion}
+Teléfono: ${telefono}
+Correo: ${correo}
+Tipo de requerimiento: ${tipo}
+Mensaje: ${mensaje}`;
+
+  let numeroWhatsApp = "593983342717";
+  let url = `https://wa.me/${numeroWhatsApp}?text=${encodeURIComponent(texto)}`;
+
+  window.open(url, "_blank");
+}
+let indiceSlide = 0;
+
+function mostrarSlide(n) {
+  let slides = document.querySelectorAll(".slide");
+  let puntos = document.querySelectorAll(".punto");
+
+  slides.forEach(slide => slide.classList.remove("activo"));
+  puntos.forEach(punto => punto.classList.remove("activo-punto"));
+
+  slides[n].classList.add("activo");
+  puntos[n].classList.add("activo-punto");
+}
+
+function moverCarrusel(direccion) {
+  let slides = document.querySelectorAll(".slide");
+
+  indiceSlide += direccion;
+
+  if (indiceSlide >= slides.length) {
+    indiceSlide = 0;
+  }
+
+  if (indiceSlide < 0) {
+    indiceSlide = slides.length - 1;
+  }
+
+  mostrarSlide(indiceSlide);
+}
+
+function slideActual(n) {
+  indiceSlide = n;
+  mostrarSlide(indiceSlide);
+}
+
+setInterval(() => {
+  moverCarrusel(1);
+}, 4000);
